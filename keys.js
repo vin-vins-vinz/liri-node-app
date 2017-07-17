3ec5415c9af51177b110ea35b79fad6afa547f8f
Liang <Nicky@Liangs-Air.fios-router.home>
2017-07-16 21:39:05 -0700

    adding

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '7u2biO4KS4LYC3I4PIEjW37nm',
+  consumer_secret: 'ZwIibNxFO3pgcxO1QY4Ha3UI91VJZgtNHa7m1T2KEK3YTHfKtt',
+  access_token_key: '886750454842933248-3VgvPOStvRoyvE4txOxxNjoz6HV53B6',
+  access_token_secret: 'u85Xe9Ua3sTnlBu64rvAYbjg7tORZc8588skv0a0Ex1Ug',
+}
