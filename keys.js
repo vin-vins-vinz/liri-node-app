2f130e1b5db8b479a21d3e857c2caecf079b202a
vin-vins-vinz <vin-vins-vinz@users.noreply.github.com>
2017-07-18 23:12:02 -0700

    Delete keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: '7u2biO4KS4LYC3I4PIEjW37nm',
-  consumer_secret: 'ZwIibNxFO3pgcxO1QY4Ha3UI91VJZgtNHa7m1T2KEK3YTHfKtt',
-  access_token: '886750454842933248-3VgvPOStvRoyvE4txOxxNjoz6HV53B6',
-  access_token_secret: 'u85Xe9Ua3sTnlBu64rvAYbjg7tORZc8588skv0a0Ex1Ug',
-}
